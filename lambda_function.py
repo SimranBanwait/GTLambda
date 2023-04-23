92dfc52130636b5f9cc1a2c4d08a68f1ad6d98ff
Simranjeet Singh <simranjeet.singh@firminiq.com>
2023-04-23 18:59:32 +0530

    major changes

--- lambda_function.py
+++ lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda! Mister White.... Science')
+        'body': json.dumps('Hello from Lambda! Yeah Mister White.... Science')
     }
